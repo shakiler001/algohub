77d480d32d6b96ebec0809a3fcaadf2b1081c9f5
shakiler001 <shakilmahmud012@gmail.com>
2023-11-19 23:51:22 +0600

    Initial

--- AlgoMain/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AlgoHub.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
